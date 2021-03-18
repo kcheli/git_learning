9c3dfafc686053ed86cc6a6daaa5dd979fc02809
kcheli <44278609+kcheli@users.noreply.github.com>
2021-03-18 18:46:23 -0400

    js mess

---
+++ js-learning/index.js
@@ -0,0 +1,38 @@
+function sum(a, b) {
+   return a + b;
+};
+
+const catalog = [
+   {
+      title: 'Overseas Trio',
+      year: '1957',
+      rating: '5/5',
+   },
+   {
+      title: 'Lonely Town',
+      year: '1959',
+      rating: '5/5',
+   },
+   {
+      title: 'Calypso',
+      year: '1977',
+      rating: '5/5',
+   },
+];
+
+let a = 3;
+let b = 4;
+let c = "twelve"; 
+
+module.exports = {
+   a: a,
+   b: b,
+   c: c,
+   sum: sum,
+   catalog: catalog,
+   values: [a, b, c, sum, catalog]
+}
+
+
+export const newVariable = "hooray it worked"
+
