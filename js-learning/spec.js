e23f93349043e5bc7db7ee21f70f34182a06c420
Kevin Cheli <44278609+kcheli@users.noreply.github.com>
2021-04-02 16:36:21 -0400

    protractor

--- js-learning/spec.js
+++ js-learning/spec.js
@@ -1,10 +1,21 @@
-const { browser } = require("protractor");
+const { browser, $, element } = require("protractor");
+
+browser.waitForAngularEnabled(false);
 
 // spec.js
 describe('Protractor Demo App', function() {
+  // beforeEach(() => {
+    
+  // })
+
   it('should have a title', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
 
+    element(by.model('first')).sendKeys(4)
+    element(by.model('second')).sendKeys(5)
+
+    $('#gobutton').click()
+    
     
     browser.sleep(5000)
     
