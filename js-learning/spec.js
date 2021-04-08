86f40b20d69b78979cc633c2e9332b7310dcc20a
kcheli <44278609+kcheli@users.noreply.github.com>
2021-04-08 17:55:57 -0400

    google tests

--- js-learning/spec.js
+++ js-learning/spec.js
@@ -1,24 +1,48 @@
-const { browser, $, element } = require("protractor");
+const { browser, $, element, ExpectedConditions } = require("protractor");
 
+let EC = ExpectedConditions;
 browser.waitForAngularEnabled(false);
 
-// spec.js
-describe('Protractor Demo App', function() {
-  // beforeEach(() => {
-    
-  // })
+describe('Protractor Demo', function() {
+  beforeEach(() => {
+    browser.get('http://www.google.com/');
+  })
 
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+  it('Elvin Jones search', async () => {
+    let inputField = element(by.name('q'));
+    browser.wait(EC.presenceOf(inputField), 2000);
+    inputField.sendKeys("elvin jones");
 
-    element(by.model('first')).sendKeys(4)
-    element(by.model('second')).sendKeys(5)
+    let searchButton = element(by.css('input[value="Google Search"]'));
+    browser.wait(EC.elementToBeClickable(searchButton), 2000);
+    searchButton.click();
 
-    $('#gobutton').click()
-    
-    
-    browser.sleep(5000)
-    
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    let wikipediaLink = element(by.css('a[href="https://en.wikipedia.org/wiki/Elvin_Jones"]'));
+    browser.wait(EC.elementToBeClickable(wikipediaLink), 2000);
+    wikipediaLink.click();
+
+    let articleHeader = $('#firstHeading')
+    browser.wait(EC.presenceOf(articleHeader), 5000);
+    let browserTitle = browser.getTitle()
+    expect(browserTitle).toBe("Elvin Jones - Wikipedia")
+  });
+
+  it('Tony Williams search', async () => {
+    let inputField = element(by.name('q'));
+    browser.wait(EC.presenceOf(inputField), 2000);
+    inputField.sendKeys("tony williams");
+
+    let searchButton = element(by.css('input[value="Google Search"]'));
+    browser.wait(EC.elementToBeClickable(searchButton), 2000);
+    searchButton.click();
+
+    let wikipediaLink = element(by.css('a[href="https://en.wikipedia.org/wiki/Tony_Williams_(drummer)"]'));
+    browser.wait(EC.elementToBeClickable(wikipediaLink), 2000);
+    wikipediaLink.click();
+
+    let articleHeader = $('#firstHeading');
+    browser.wait(EC.presenceOf(articleHeader), 5000);
+    let browserTitle = browser.getTitle();
+    expect(browserTitle).toBe("Tony Williams (drummer) - Wikipedia");
   });
 });
